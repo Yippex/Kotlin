1b6f4e85314092fedafb5d06b896071f2d6da0c4
Yippex <danieldashe90@gmail.com>
2024-06-06 00:42:23 -0700

    first commit

---
+++ src/Main.kt
@@ -0,0 +1,10 @@
+fun main(){
+    println(" Hello Kt")
+    val num = 40
+    println(num)
+    var x =  90
+    println(" The value of x is: $x")
+    var Str = "Kotlin is the best language"
+    println(Str)
+
+}
